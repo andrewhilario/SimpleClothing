9d44289ab603c8cc39c03a29cd81727ed6aff17d
Embellish <72259687+drodols12@users.noreply.github.com>
2021-05-16 11:29:41 +0800

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+// var timeout = 2000;
+// function slideshow(){
+//     const slide = document.getElementById("slide");
+//     let img = ['0%','-101%','-202%','-303%','-404%'];
+
+
+
+//     img.forEach((slides, index)=>{
+//       setTimeout(()=>{
+//             slide.style.marginLeft = slides;
+//             console.log(index)
+//         }, index * timeout);
+//         if(index <= 4){
+//             slide.style.transform = "translateX(0%)";
+//             slide.removeAttribute("margin-left");
+//         }
+        
+//     })
+
+// }
+// slideshow();
